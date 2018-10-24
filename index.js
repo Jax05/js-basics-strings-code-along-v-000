bf110ca4b0f96adee7ce57adcb3bda7f452a4bbf
Carley Tripp <carley.tripp@outlook.com>
2018-10-24 22:36:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 let currentUser = "Avi";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
